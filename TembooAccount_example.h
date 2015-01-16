c2e7f1d81245cdd58a68f83aa2009568d134872a
chadallen <cga@chadallen.net>
2015-01-15 19:38:24 -0800

    updated .h file names

---
+++ TembooAccount_example.h
@@ -0,0 +1,3 @@
+#define TEMBOO_ACCOUNT "yourname"  // your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "x"  // your Temboo app key name
+#define TEMBOO_APP_KEY  "x"  // your Temboo app key
---
+++ TwilioAccount_example.h
@@ -0,0 +1,4 @@
+#define TWILIO_ACCOUNT_SID "x"  // the Account SID from your Twilio account
+#define TWILIO_AUTH_TOKEN "x"  // the Auth Token from your Twilio account
+#define TWILIO_NUMBER  "+1 555-555-5555"  // your Twilio phone number
+#define RECIPIENT_NUMBER  "+1 555-555-5555"  // the number to which the SMS should be sent
